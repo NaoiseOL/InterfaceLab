d18ef6aea0249c2ed4b0a523e5de9c1005e8cf71
naoise <G00385498@atu.ie>
2024-02-23 16:58:22 +0000

    Populated the classes Burger, Pizza and Salad

--- src/main/java/ie/atu/Pizza.java
+++ src/main/java/ie/atu/Pizza.java
@@ -1,4 +1,29 @@
 package ie.atu;
 
-public class Pizza {
+public class Pizza implements MenuItem{
+    private String name;
+    private double price;
+    private String toppings;
+
+    public Pizza(String name, double price, String toppings){
+        this.name=name;
+        this.price=price;
+        this.toppings=toppings;
+    }
+
+    @Override
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public double getPrice(){
+        return price;
+    }
+
+    @Override
+    public String getDescription(){
+        return toppings;
+    }
+
 }
